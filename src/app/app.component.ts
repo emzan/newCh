6b2161120dfdd9e721bf8676e4f424643d3a3b77
emzan <zanettiemilio@hotmail.com>
2020-03-11 08:32:54 +0800

    indicumRovarum

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,12 +20,12 @@ import { UserData } from './providers/user-data';
 export class AppComponent implements OnInit {
   appPages = [
     {
-      title: 'Schedule',
+      title: 'Words',
       url: '/app/tabs/schedule',
       icon: 'calendar'
     },
     {
-      title: 'Speakers',
+      title: 'Index',
       url: '/app/tabs/speakers',
       icon: 'people'
     },
